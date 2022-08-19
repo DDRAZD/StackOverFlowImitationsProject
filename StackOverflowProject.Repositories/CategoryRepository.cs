8eae1fb541460172448dfa1a06daa9d26213e313
Yaniv Nathan <yanivnathan@gmail.com>
2022-08-19 13:03:25 -0400

    added repository for category and question

---
+++ StackOverflowProject.Repositories/CategoryRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowProject.DomainModels;
+
+namespace StackOverflowProject.Repositories
+{
+    public interface ICategoriesRepository
+    {
+        List<Category> GetCategories();
+        List<Category> GetCategoryByID(int id);
+        void DeleteCategory(int id);
+        void UpdateCategory(Category category);
+        void InsertCategory(Category category);
+
+    }
+    public class CategoryRepository : ICategoriesRepository
+    {
+        StackOverflowDatabaseDBContext db;
+        //constructor
+        public CategoryRepository()
+        {
+            db = new StackOverflowDatabaseDBContext();
+        }
+
+        public void DeleteCategory(int id)
+        {
+           Category categoryToDelete = db.Categories.Where(y=>y.CategoryID == id).FirstOrDefault();
+            if(categoryToDelete != null)
+            {
+                db.Categories.Remove(categoryToDelete);
+                db.SaveChanges();
+            }
+                
+        }
+
+        public List<Category> GetCategories()
+        {
+            return db.Categories.ToList();
+        }
+
+        public List<Category> GetCategoryByID(int id)
+        {
+            List<Category> category = db.Categories.Where(y=>y.CategoryID == id).ToList();
+            return category;
+        }
+
+        public void InsertCategory(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+           Category categoryToUpdate = db.Categories.Where(y=>y.CategoryID == category.CategoryID).FirstOrDefault();
+            if(categoryToUpdate!=null)
+            {
+                categoryToUpdate.CategoryName = category.CategoryName;
+                db.SaveChanges();
+
+            }
+           
+        }
+    }
+}
---
+++ StackOverflowProject.Repositories/QuestionsRepository.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowProject.DomainModels;
+
+
+namespace StackOverflowProject.Repositories
+{
+    public interface IQuestionsRepository
+    {
+        void InsertQuestion(Question question);
+        void UpdateQuestionDetails(Question question);
+        /// <summary>
+        /// this will update the counter with the value of the vote - it will be 1 or -1
+        /// </summary>        
+        void UpdateQuestionVoteCounter(int qid, int value);
+        /// <summary>
+        /// if new answer is added it will be +1 if deleted it will be -1
+        /// </summary>
+        /// <param name="qid"></param>
+        /// <param name="value"></param>
+        void UpdateQuestionAnswersCounter(int qid, int value);
+
+        /// <summary>
+        /// this will only increment
+        /// </summary>
+        /// <param name="qid"></param>
+        void UpdateQuestionViewsCounter(int qid);
+        void DeleteQuestion(int qid);
+        List<Question> GetAllQuestions();
+        List<Question> GetQuestionByQuestionID(int qid);
+    }
+    public class QuestionsRepository : IQuestionsRepository
+    {
+        StackOverflowDatabaseDBContext db;
+
+        //constructor (can use later depenancy injection)
+
+        public QuestionsRepository()
+        {
+            db = new StackOverflowDatabaseDBContext();
+        }
+        public void DeleteQuestion(int qid)
+        {
+            Question questionToDelete = db.Questions.Where(y=>y.QuestionID == qid).FirstOrDefault();
+            if (questionToDelete != null)
+            {
+                db.Questions.Remove(questionToDelete);
+                db.SaveChanges();
+            }
+        }
+
+        public List<Question> GetAllQuestions()
+        {
+            return db.Questions.OrderByDescending(y=>y.QuestionDateAndTime).ToList();
+        }
+
+        public List<Question> GetQuestionByQuestionID(int qid)
+        {
+            List<Question> questions= db.Questions.Where(y=>y.QuestionID ==qid).ToList();
+
+           return questions;
+        }
+
+        public void InsertQuestion(Question question)
+        {
+            db.Questions.Add(question);
+            db.SaveChanges();
+        }
+
+        public void UpdateQuestionAnswersCounter(int qid, int value)
+        {
+            Question q = db.Questions.Where(y => y.QuestionID == qid).FirstOrDefault();
+            if (q != null)
+            {
+                q.AnswersCount = q.AnswersCount + value;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateQuestionDetails(Question question)
+        {
+            Question q = db.Questions.Where(y => y.QuestionID == question.QuestionID).FirstOrDefault();
+            if (q != null)
+            {
+                q.QuestionName =question.QuestionName;
+                q.QuestionDateAndTime = question.QuestionDateAndTime;
+                q.CategoryID = question.CategoryID;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateQuestionViewsCounter(int qid)
+        {
+            Question q = db.Questions.Where(y => y.QuestionID == qid).FirstOrDefault();
+            if (q != null)
+            {
+                q.ViewsCounter++; ;
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateQuestionVoteCounter(int qid, int value)
+        {
+            Question q = db.Questions.Where(y => y.QuestionID == qid).FirstOrDefault();
+            if(q != null)
+            {
+                q.VotesCount = q.VotesCount + value;
+                db.SaveChanges();
+            }
+        }
+    }
+}

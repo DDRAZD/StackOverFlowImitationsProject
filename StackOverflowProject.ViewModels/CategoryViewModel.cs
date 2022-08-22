c209f48df9561b8bb5d0586e2072d42b802fa065
Yaniv Nathan <yanivnathan@gmail.com>
2022-08-22 11:35:43 -0400

    completed the domain models installed automapper to service layer

---
+++ StackOverflowProject.ViewModels/CategoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflowProject.ViewModels
+{
+    /// <summary>
+    /// used to manipulate category in the view, such as edit, inserting deleting categories etc. 
+    /// </summary>
+    public class CategoryViewModel
+    {
+        [Required]
+        public int CategoryID { get; set; }
+        [Required]
+        public string CategoryName { get; set; }   
+    }
+}
---
+++ StackOverflowProject.ViewModels/QuestionViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflowProject.ViewModels
+{
+    /// <summary>
+    /// this view model is for the users to be able to view the question - these are the data elements we wish to show
+    /// </summary>
+    public class QuestionViewModel
+    {
+        [Required]
+        public int QuestionID { get; set; }
+        [Required]
+        public string QuestionName { get; set; }
+        [Required]
+        public DateTime QuestionDateAndTime { get; set; }
+        [Required]
+        public int UserID { get; set; }
+        [Required]
+        public int CategoryID { get; set; }
+        [Required]
+        public int VotesCount { get; set; }
+        [Required]
+        public int AnswersCount { get; set; }
+        [Required]
+        public int ViewsCount { get; set; }
+
+        public virtual UserViewModel User { get; set; }
+        public virtual CategoryViewModel Category { get; set; }
+        public virtual List<AnswerViewModel> Answers { get; set; }
+    }
+}

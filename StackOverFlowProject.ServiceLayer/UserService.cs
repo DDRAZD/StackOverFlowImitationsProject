66080f6c181200d6aece4148381cf6b04bda1b4a
Yaniv Nathan <yanivnathan@gmail.com>
2022-08-23 07:54:57 -0400

    added automapper added SHA256 hash

---
+++ StackOverFlowProject.ServiceLayer/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackOverflowProject.DomainModels;
+using StackOverflowProject.ViewModels;
+using StackOverflowProject.Repositories;
+using AutoMapper;
+using AutoMapper.Configuration;
+
+namespace StackOverFlowProject.ServiceLayer
+{
+    public interface IUserservice
+    {
+      //  IUsersRepository UsersRepository { get; }
+
+
+    }
+    public class UserService: IUserservice
+    {
+    }
+}

5a26c3c1a625c92f147e83d0265ddf8bf8f969a2
Yaniv Nathan <yanivnathan@gmail.com>
2022-08-29 08:00:30 -0400

    added bundles and layout page - still debugging bundle loading

---
+++ StackOverFlowImitationsProject/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace StackOverFlowImitationsProject
+{
+    public class BundleConfig
+    {
+        /// <summary>
+        /// this method needs to be invoked in the global.asax file with " App_Start.BundleConfig.RegisterBundles(BundleTable.Bundles);"
+        /// </summary>
+        /// <param name="bundles"></param>
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundle names are user defined
+
+            //creating a bundle called "bootstrap" in scripts, comprising of 3 js files that are sufficient to run bootstrap
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include("~/Scripts/jquery-3.6.0.js", "~/Scripts/umd/popper.js", "~/Scripts/bootstrap.js"));
+        
+            //creating a bundle for styles
+            bundles.Add(new StyleBundle("~/Styles/bootstrap").Include("~/Content/bootstrap.css"));
+
+            //you are free to add styles or scrips as you wish to the bundles, but the above are the base ones for bootstrap
+            //for example, add a Web file in "Content" of the main application names Styles.css, then create a bundle for it:
+            bundles.Add(new StyleBundle("~/Styles/site").Include("~/Content/Sytles.css"));
+
+            //after creation of the bundles, we need to enable optimization:
+            BundleTable.EnableOptimizations = true;
+
+        }
+    }
+}

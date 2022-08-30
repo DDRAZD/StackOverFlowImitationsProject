1f214f587bb39f813de14a88af9c74a7499976d0
Yaniv Nathan <yanivnathan@gmail.com>
2022-08-30 07:21:42 -0400

    added dependancy injection for questions - available for both api and mvc

---
+++ StackOverFlowImitationsProject/App_Start/UnityConfig.cs
@@ -0,0 +1,33 @@
+using System.Web.Http;
+using Unity;
+using Unity.WebApi;
+using Unity.Mvc5;
+using System.Web.Mvc;
+using StackOverFlowProject.ServiceLayer;
+
+namespace StackOverFlowImitationsProject
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+
+            //we will create a container and register the types
+
+            container.RegisterType<IQuestionService, QuestionService>(); //every time you see IQuestino, swap it at run time with Question
+            DependencyResolver.SetResolver(new Unity.Mvc5.UnityDependencyResolver(container));//because we have both WebAPI and MVC dependancy injection, we need 
+            //to align them, hence we are sending the MVC resolver to the other resolver
+
+            GlobalConfiguration.Configuration.DependencyResolver = new Unity.WebApi.UnityDependencyResolver(container);//this allows dependancy injection for WebAPI
+
+            
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+            
+            // e.g. container.RegisterType<ITestService, TestService>();
+            
+           
+        }
+    }
+}
--- StackOverFlowImitationsProject/Controllers/HomeController.cs
+++ StackOverFlowImitationsProject/Controllers/HomeController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StackOverFlowProject.ServiceLayer;
+using StackOverflowProject.ViewModels;
+
 
 
 namespace StackOverFlowImitationsProject.Controllers
 {
     public class HomeController : Controller
     {
+        IQuestionService qs;
+
+
+        //constructor
+
+        public HomeController(IQuestionService qs)
+        {
+            this.qs = qs; //dependnacy injection
+        }
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            //dsiplaying the latest questions
+            List<QuestionViewModel> questions =  this.qs.GetAllQuestions().Take(10).ToList(); //taking just the top 10
+            return View(questions);
         }
     }
 }

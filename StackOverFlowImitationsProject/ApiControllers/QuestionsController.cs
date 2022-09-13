d65b160fbc6219cb20c81735c3536d0618fd3a1c
Yaniv Nathan <yanivnathan@gmail.com>
2022-09-13 09:25:05 -0400

    added voting mechanim, JS and Ajax

---
+++ StackOverFlowImitationsProject/ApiControllers/QuestionsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StackOverflowProject.ServiceLayer;
+using StackOverflowProject.ViewModels;
+
+namespace StackOverFlowImitationsProject.ApiControllers
+{
+    public class QuestionsController : ApiController
+    {
+        
+        IAnswersService ans;
+
+        //constructor
+
+        public QuestionsController(IAnswersService ans)
+        {
+           
+            this.ans = ans;
+        }
+
+        public void Post(int AnswerID, int UserID, int Value)
+        {
+            this.ans.UpdateAnswerVotesCount(AnswerID, UserID, Value);
+        }
+    }
+}
